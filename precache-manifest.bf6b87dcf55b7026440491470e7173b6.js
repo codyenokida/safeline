31a3bb4a7a1acb3661de6dad1eed2c4f83d65242
codyenokida <codyenokida@gmail.com>
2019-02-16 17:24:12 -0800

    Updates

---
+++ precache-manifest.bf6b87dcf55b7026440491470e7173b6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1da0e6d142a266bcbb747b909346269",
+    "url": "/safeline/static/media/safeline_logo1.c1da0e6d.png"
+  },
+  {
+    "revision": "9adab284782e55cf7ce408f44aa8302e",
+    "url": "/safeline/static/media/safeline_logo.9adab284.png"
+  },
+  {
+    "revision": "2e92fc492b58385ad60eaf08bc38938d",
+    "url": "/safeline/static/media/login_screen.2e92fc49.jpg"
+  },
+  {
+    "revision": "1b8f5c94040e02ac220c40d729c4aedb",
+    "url": "/safeline/static/media/blurry_background.1b8f5c94.jpg"
+  },
+  {
+    "revision": "331b3736a7950a09e5a2c33fc8d64140",
+    "url": "/safeline/static/media/Walkway.331b3736.ttf"
+  },
+  {
+    "revision": "e567c9004e5098528ad5",
+    "url": "/safeline/static/js/runtime~main.e567c900.js"
+  },
+  {
+    "revision": "30662289836db62f448b",
+    "url": "/safeline/static/js/main.30662289.chunk.js"
+  },
+  {
+    "revision": "976f5f75794b8eca4432",
+    "url": "/safeline/static/js/2.976f5f75.chunk.js"
+  },
+  {
+    "revision": "30662289836db62f448b",
+    "url": "/safeline/static/css/main.2fad50b7.chunk.css"
+  },
+  {
+    "revision": "770d548b1fcc33a38aa9fd9d454cd0ec",
+    "url": "/safeline/index.html"
+  }
+];
